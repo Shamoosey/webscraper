ecd50fdf5e10d729e803c799c87e46999ca2d23f
shamuso <shamuso23@gmail.com>
2022-02-09 20:29:54 -0600

    Updated stuff

--- src/WebScraper.ts
+++ src/WebScraper.ts
@@ -1,23 +1,20 @@
 import {inject, injectable} from "inversify";
 import { Scraper } from "./interfaces";
-import axios from "axios"
 import Puppeteer, { Browser } from "puppeteer";
 
 @injectable()
 export class WebScraper implements Scraper.IWebScraper {
     constructor () {
-        
     }
     
-    private url = "https://www.memoryexpress.com/Category/VideoCards?PageSize=120"
+    private url = ""
 
     public async Run(): Promise<void>{
         let browser = await this.initPuppeteerBrowser();
 
         let page = await browser.newPage()
         await page.goto(this.url, {waitUntil: `load`});
-        let data = await page.$eval('.c-shca-container', el => el.innerHTML)
-        console.log(data);
+
     }
 
 
