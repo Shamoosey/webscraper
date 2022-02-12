7a0c66e0f93f790cee391b5c93da0d43904e4b19
shamuso <shamuso23@gmail.com>
2022-02-12 17:38:32 -0600

    Updated logger to export static class

--- src/dependencyInjection.ts
+++ src/dependencyInjection.ts
@@ -4,14 +4,14 @@ import { WebScraper } from "./WebScraper"
 import { BrowserHelper } from "./BrowserHelper";
 import { ScraperMock } from "./Mocks/ScraperMock";
 import { Scraper } from "./interfaces"
-import logger  from "./logger";
 import { Logger } from "winston";
+import { LogFactory } from "./LogFactory";
 
 let container = new Container();
 
 container.bind<Scraper.IWebScraper>("WebScraper").to(WebScraper).inSingletonScope();
 container.bind<Scraper.IBrowserHelper>("BrowserHelper").to(BrowserHelper).inSingletonScope();
 container.bind<Scraper.IScraperMock>("ScraperMock").to(ScraperMock).inSingletonScope();
-container.bind<Logger>("Logger").toConstantValue(logger)
+container.bind<Logger>("Logger").toConstantValue(LogFactory.GetNewLogger());
 
 export default container;
